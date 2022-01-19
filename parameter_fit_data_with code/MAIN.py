b6a2895db6e3868720850896b33be312dcb66cbf
jafrizzell <j.a.frizzell@hotmail.com>
2022-01-18 20:26:35 -0600

    Added general MAIN.py program to allow for fast prediction of RAOs

--- parameter_fit_data_with code/MAIN.py
+++ parameter_fit_data_with code/MAIN.py
@@ -1,14 +1,24 @@
 from RaoPrediction_class import RaoPrediction
 
-length = 40
-beam = 22
-draft = 2.5
-heading = 180
+# Start inputs
 
-model_path = "D:/IdeaProjects/PyCharm/TAMU_Work/OCEN 485/damped_spring_all_dir3"
+length = 40  # Barge length
+beam = 22  # Barge beam
+draft = 2.5  # Barge draft
+heading = 180  # Wave heading, 0 degrees is head seas, 90 is beam
+
+model_path = "D:/IdeaProjects/PyCharm/TAMU_Work/OCEN 485/damped_spring_all_dir3"  # path the the trained NN model directory
+
+low_freq = 0.1  # lowest wave frequency to predict the RAOs at
+high_freq = 2.5  # highest wave frequency to predict the RAOs at
+n_points = 25  # Number of points to predict the RAOs at, higher = more resolution in plot
+
+# End Inputs
+
+# Initialize rao class structure and predict RAOs with given inputs
 
 rao = RaoPrediction()
 
 rao.dnn(model_path)
 rao.predict(length, beam, draft, heading)
-rao.visualize(0.1, 2.5, 25)
+rao.visualize(low_freq, high_freq, n_points)
