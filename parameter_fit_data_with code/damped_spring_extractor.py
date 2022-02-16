18a4f36ad42baac62eb5f791d3dcda42754d88a5
jafrizzell <j.a.frizzell@hotmail.com>
2022-02-16 16:13:50 -0600

    Edited Paper, added todos in code

--- parameter_fit_data_with code/damped_spring_extractor.py
+++ parameter_fit_data_with code/damped_spring_extractor.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import numpy as np
 import os
-from math import e
+from math import e, sqrt
 from scipy.optimize import curve_fit
-import shutil
-import matplotlib.pyplot as plt
-import sklearn as sklearn
+from statistics import mean
 
-from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
-base_path = 'D:/TAMU Work/TAMU 2021 FALL/OCEN 485/Analysis Data/small_barge/'
+base_path = 'D:/TAMU Work/TAMU 2021 FALL/OCEN 485/Analysis Data/med_barge/'
 
-output_file = pd.read_csv("D:/TAMU Work/TAMU 2021 FALL/OCEN 485/Analysis Data/small_barge_data.csv", sep=',')
+output_file = pd.read_csv("D:/TAMU Work/TAMU 2021 FALL/OCEN 485/Analysis Data/mid_size_barges.csv", sep=',')
 writer = []
 writer_data = pd.DataFrame()
 
@@ -29,55 +27,68 @@ for subdirs, dirs, files in os.walk(base_path):
     for file in files:
         print("Fitting curve from file ", count)
         data_holder = []
-        data_holder2 = []
-        data_holder3 = []
+
         filedata = open(base_path+file).readlines()
         for i in range(len(filedata)):
             if "R.A.O.S-VARIATION WITH WAVE PERIOD/FREQUENCY" in filedata[i] and "VEL" not in filedata[i] and "ACC" not in filedata[i]:
                 data_holder.append(np.array(filedata[i+7:i+27]))
-        try:
-            data_holder = data_holder[2]
-
-            for j in data_holder:
-                data_holder2.append(j.tolist().split('\n')[0])
-            for k in data_holder2:
-                new_split = k.split(' ')
-                for item in new_split:
-                    try:
-                        item = float(item)
-                        data_holder3.append(item)
-                    except ValueError:
-                        pass
-            heading = data_holder3[2]
-            data_holder3.pop(2)
-            data_holder3 = np.asarray(data_holder3).reshape(20,14)
-            data = pd.DataFrame(data_holder3)
-
-            data.drop([3, 5, 7, 9, 11, 13], axis=1)
-            line = []
-            l = output_file['Length (m)'][count]
-            b = output_file['Beam (m)'][count]
+                data_holder.append(np.array(filedata[i+28:i+48]))
+        for a in range(5):
+            data_holder2 = []
+            data_holder3 = list()
             try:
-                d = float(output_file['Draft (m)'][count])
-                line.extend([l, b, d, heading])
-                for i in range(6):
-                    print('Fitting DoF ', i+1)
-                    try:
-                        coeffs, cov = curve_fit(func, data[1], data[2+2*i])
-                        predicted = func(data[1], *coeffs)
-                        r_squared = [float(r2_score(data[2+2*i], predicted))]
-                        coeffs = list(coeffs)
-                        coeffs.extend(r_squared)
-                        line.extend(coeffs)
-                    except RuntimeError:
-                        line.extend(['na', 'na', 'na', 'na'])
-            except ValueError:
+                data_holder_t = data_holder[a]
+                for j in data_holder_t:
+                    data_holder2.append(j.tolist().split('\n')[0])
+                for k in data_holder2:
+                    new_split = k.split(' ')
+                    for item in new_split:
+                        try:
+                            item = float(item)
+                            data_holder3.append(item)
+                        except ValueError:
+                            pass
+                heading = data_holder3[2]
+                data_holder3.pop(2)
+                data_holder3 = np.asarray(data_holder3).reshape(20,14)
+                data = pd.DataFrame(data_holder3)
+
+                data.drop([3, 5, 7, 9, 11, 13], axis=1)
+                line = []
+                l = output_file['Length (m)'][count]
+                b = output_file['Beam (m)'][count]
+                try:
+                    d = float(output_file['Draft (m)'][count])
+                    line.extend([l, b, d, heading])
+                    for i in range(6):
+                        print('Fitting DoF ', i+1)
+                        try:
+                            coeffs, cov = curve_fit(func, data[1], data[2+2*i])
+                            predicted = func(data[1], *coeffs)
+                            rmse = [float(sqrt(mean_squared_error(data[2+2*i], predicted)))]
+                            m = [float(mean(predicted))]
+                            coeffs = list(coeffs)
+                            coeffs.extend(rmse)
+                            coeffs.extend(m)
+                            line.extend(coeffs)
+                        except RuntimeError:
+                            line.extend(['na', 'na', 'na', 'na', 'na'])
+                except ValueError:
+                    pass
+                writer.append(line)
+            except IndexError:
                 pass
-            writer.append(line)
-        except IndexError:
-            pass
 
         count += 1
     writer_data = pd.DataFrame(writer)
 
-writer_data.to_csv(base_path+'small_data6.csv')
+col = ['Length', 'Beam', 'Draft', 'Heading',
+           'Asurge', 'Bsurge', 'Csurge', 'RMSEsurge', 'Msurge',
+           'Asway', 'Bsway', 'Csway', 'RMSEsway', 'Msway',
+           'Aheave', 'Bheave', 'Cheave', 'RMSEheave', 'Mheave',
+           'Aroll', 'Broll', 'Croll', 'RMSEroll', 'Mroll',
+           'Apitch', 'Bpitch', 'Cpitch', 'RMSEpitch', 'Mpitch',
+           'Ayaw', 'Byaw', 'Cyaw', 'RMSEyaw', 'Myaw']
+
+writer_data.columns = col
+writer_data.to_csv(base_path+'mid_data8.csv', index=False)
