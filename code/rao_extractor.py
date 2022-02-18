cc2c5bce4be846ab658f319915718b5651e77e4a
jafrizzell <j.a.frizzell@hotmail.com>
2022-02-17 19:19:47 -0600

    Cleaned up files, deleted old models.

--- code/rao_extractor.py
+++
@@ -1,9 +0,0 @@
-import scipy
-import pandas
-
-raw_data = \
-    open('C:\Users\jafri\Documents\GitHub\RAO-Research\actual_real_rao_files\dp0\AQW\AQW\AQ\Analysis\ANALYSIS.LIS')
-
-print(raw_data.readlines())
-
-print()
