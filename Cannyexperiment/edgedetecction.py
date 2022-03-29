843eed531a2f9d7bea6d5271cde275bf6ff434d8
willjblech <98859273+willjblech@users.noreply.github.com>
2022-03-29 10:47:46 -0500

    Edited Paper, added todos in code

--- Cannyexperiment/edgedetecction.py
+++ Cannyexperiment/edgedetecction.py
@@ -7,4 +7,27 @@ Vid = cv.VideoCapture('cannystillframe.mp4')
 if (Vid.isOpen() == False):
     print('Error vid closed')
 else:
-    fps = int(Vid.get(5)))
+    fps = int(Vid.get(5))
+    print("Frame Rate : ",fps,"frames per second")
+    frame_count = Vid.get(7)
+    print("Frame count : ", frame_count)
+
+while(Vid.isOpened()):
+    # vCapture.read() methods returns a tuple, first element is a bool
+    # and the second is frame
+
+    ret, frame = Vid.read()
+    if ret == True:
+        cv.imshow('Frame',frame)
+        k = cv.waitKey(20)
+        # 113 is ASCII code for q key
+        if k == 113:
+            break
+    else:
+        break
+
+# Obtain frame size information using get() method
+frame_width = int(Vid.get(3))
+frame_height = int(Vid.get(4))
+frame_size = (frame_width,frame_height)
+fps = fps
