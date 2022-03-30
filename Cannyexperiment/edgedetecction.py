e8cec47bf99d13ad6af8f0dac225d847d14a2074
willjblech <98859273+willjblech@users.noreply.github.com>
2022-03-30 11:39:53 -0500

    Edited Paper, added todos in code

--- Cannyexperiment/edgedetecction.py
+++ Cannyexperiment/edgedetecction.py
@@ -1,5 +1,50 @@
 from __future__ import print_function
+from __future__ import division
 import cv2 as cv
+import numpy as np
+import argparse
+from math import atan2, cos, sin, sqrt, pi
+
+def drawAxis(img, p_, q_, colour, scale):
+    p = list(p_)
+    q = list(q_)
+
+    angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+    hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+    # Here we lengthen the arrow by a factor of scale
+    q[0] = p[0] - scale * hypotenuse * cos(angle)
+    q[1] = p[1] - scale * hypotenuse * sin(angle)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+    # create the arrow hooks
+    p[0] = q[0] + 9 * cos(angle + pi / 4)
+    p[1] = q[1] + 9 * sin(angle + pi / 4)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+    p[0] = q[0] + 9 * cos(angle - pi / 4)
+    p[1] = q[1] + 9 * sin(angle - pi / 4)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+
+def getOrientation(pts, img):
+
+    sz = len(pts)
+    data_pts = np.empty((sz, 2), dtype=np.float64)
+    for i in range(data_pts.shape[0]):
+        data_pts[i,0] = pts[i,0,0]
+        data_pts[i,1] = pts[i,0,1]
+    # Perform PCA analysis
+    mean = np.empty((0))
+    mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
+    # Store the center of the object
+    cntr = (int(mean[0,0]), int(mean[0,1]))
+
+
+    cv.circle(img, cntr, 3, (255, 0, 255), 2)
+    p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+    p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+    drawAxis(img, cntr, p1, (0, 255, 0), 1)
+    drawAxis(img, cntr, p2, (255, 255, 0), 5)
+    angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+
+    return angle
 
 #backSub = cv.createBackgroundSubtractorMOG2()
 backSub = cv.createBackgroundSubtractorKNN()
@@ -36,55 +81,27 @@ while capture.isOpened():
     cv.imshow('Frame', frame)
     cv.imshow('FG Mask', fgMask)
 
-    ret, thresh = cv.threshold(fgMask, 150, 255, cv.THRESH_BINARY)
-    # detect the contours on the binary image using cv2.ChAIN_APPROX_SIMPLE
-    contours1, hierarchy1 = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # draw contours on the original image for `CHAIN_APPROX_SIMPLE`
-    image_copy1 = frame.copy()
-    cv.drawContours(image_copy1, contours1, -1, (0, 255, 0), 2, cv.LINE_AA)
-    # see the results
-    cv.imshow('Simple approximation', image_copy1)
+    # Convert image to binary
+    _, bw = cv.threshold(fgMask, 50, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    contours, _ = cv.findContours(bw, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+    for i, c in enumerate(contours):
+        # Calculate the area of each contour
+        area = cv.contourArea(c)
+        # Ignore contours that are too small or too large
+        if area < 1e2 or 1e5 < area:
+            continue
+        # Draw each contour only for visualisation purposes
+        cv.drawContours(frame, contours, i, (0, 0, 255), 2)
+        # Find the orientation of each shape
+        getOrientation(c, frame)
+
+    cv.imshow('output', frame)
 
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
         break
+#print(contours1)
 
-'''# read image
-img = cv.imread('cannystillframe_Moment.jpg')
+cv.waitKey()
 
-# convert the image to grayscale format
-img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-# apply binary thresholding
-ret, thresh = cv.threshold(img_gray, 150, 255, cv.THRESH_BINARY)
-# visualize the binary image
-cv.imshow('Binary image', thresh)
-cv.waitKey(0)
-cv.imwrite('image_thres1.jpg', thresh)
-cv.destroyAllWindows()
-
-# detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
-contours, hierarchy = cv.findContours(image=thresh, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
-
-# draw contours on the original image
-image_copy = img.copy()
-cv.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv.LINE_AA)
-
-# see the results
-cv.imshow('None approximation', image_copy)
-cv.waitKey(0)
-cv.imwrite('contours_none_image1.jpg', image_copy)
-cv.destroyAllWindows()
-"""
-Now let's try with `cv2.CHAIN_APPROX_SIMPLE`
-"""
-# detect the contours on the binary image using cv2.ChAIN_APPROX_SIMPLE
-contours1, hierarchy1 = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-# draw contours on the original image for `CHAIN_APPROX_SIMPLE`
-image_copy1 = img.copy()
-cv.drawContours(image_copy1, contours1, -1, (0, 255, 0), 2, cv.LINE_AA)
-# see the results
-cv.imshow('Simple approximation', image_copy1)
-cv.waitKey(0)
-cv.imwrite('contours_simple_image1.jpg', image_copy1)
-cv.destroyAllWindows()'''
